07982f5d51428c7707090516c55a565fab2b8adf
ayushmishra333 <ayushmishra333@gmail.com>
2025-02-11 07:39:35 +0000

    Added details page

---
+++ src/pages/product/[id].js
@@ -0,0 +1,38 @@
+import { useRouter } from "next/router";
+import { useProductContext } from "../../context/ProductContext";
+import Link from "next/link";
+
+export default function ProductDetail() {
+    const { push, query } = useRouter();
+    const { products, dispatch } = useProductContext();
+    const product = products.find((p) => p.id === parseInt(query.id));
+
+    if (!product) return <p>Product not found.</p>;
+
+    return (
+        <div>
+            <div>{product.title}</div>
+            <div>£{product.price}</div>
+            <div>{product.description}</div>
+            <span>Brand</span> <span>{product.brand}</span>
+            <span>Rating</span> <span>{product.rating}</span>
+            <span>Current stock</span> <span>{product.stock}</span>
+            <div>
+                <Link href={`/product/edit/${product.id}`}>
+                    <button>Edit</button>
+                </Link>
+                <button
+                    onClick={() => {
+                        dispatch({ type: "DELETE", payload: product.id });
+                        push("/");
+                    }}
+                >
+                    Delete
+                </button>
+            </div>
+            <div>
+                <img src={product.thumbnail} width={"500px"} />
+            </div>
+        </div>
+    );
+}
